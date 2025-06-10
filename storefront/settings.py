d3651c23bd52de18e669bc5fa20993af63a15be5
PenDatabase <davideneasatochibueze@gmail.com>
2025-06-10 23:45:54 +0100

    fix: changed db to use sqlite

--- storefront/settings.py
+++ storefront/settings.py
@@ -94,12 +94,8 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
